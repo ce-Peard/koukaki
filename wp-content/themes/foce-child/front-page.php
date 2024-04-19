690742e3f6fef90b24da96f68f058065edb200b5
ce-Peard <cepeard.cep@gmail.com>
2024-04-19 18:31:40 +0200

    Ajouts section Oscars

--- wp-content/themes/foce-child/front-page.php
+++ wp-content/themes/foce-child/front-page.php
@@ -49,6 +49,15 @@ get_header();
             <p>Avec une créativité et une capacité d’innovation mondialement reconnues, une expertise éditoriale et commerciale à la pointe de son industrie, le Studio Koukaki se positionne comme un acteur incontournable dans un marché en forte croissance. Koukaki construit chaque année de véritables succès et capitalise sur de puissantes marques historiques. Cette année, il vous présente “Fleurs d’oranger et chats errants”.</p>
         </div>
     </section>
+    <section id="oscars" class="oscars">
+            <div class="oscars__title">
+            <img src="<?php echo get_theme_file_uri() . '/assets/images/fond_orange_oscars.png'; ?>" alt="fond du titre des nominations">
+            <h3>Fleurs d’oranger & chats errants est nominé aux Oscars Short Film Animated de 2022 !</h3>
+            </div>
+            <div class="oscars__image">
+                <img src="<?php echo get_theme_file_uri() . '/assets/images/oscars.png'; ?>" alt="Nomination aux Oscars 2022" ">
+            </div>
+        </section>
 </main><!-- #main -->
 
 <?php
