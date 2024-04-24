8d6397ef24f710fdc50ad1972fcde5249dcf896e
ce-Peard <cepeard.cep@gmail.com>
2024-04-24 14:32:21 +0200

    menu-ouvert animations Histoire

--- wp-content/themes/foce-child/template_parts/menu-ouvert.php
+++ wp-content/themes/foce-child/template_parts/menu-ouvert.php
@@ -1,14 +1,11 @@
-<div>
-    <img class="menu_ouvert_logo" src="<?php echo get_theme_file_uri() . '/assets/images/image_logo_en_paralax.png'; ?>" alt="">
-    <ul class="menu_ouvert_contenu">
-        <li class="modal__content--story flower cat"><a href="#story">Histoire</a></li>
-        <li class="modal__content--characters flower cat"><a href="#characters">Personnages</a></li>
-        <li class="modal__content--place flower cat"><a href="#place">Lieu</a></li>
-        <li class="modal__content--studio flower cat"><a href="#studio">Studio Koukaki</a>
-        </li>
-    </ul>
-
-    <div class="menu_ouvert_footer modal-trigger flower cat">
-        <a href="#">STUDIO KOUKAKI</a></li>
-    </div>
+<img class="menu_ouvert_logo" src="<?php echo get_theme_file_uri() . '/assets/images/image_logo_en_paralax.png'; ?>" alt="">
+<ul class="menu_ouvert_contenu">
+    <li class="menu_ouvert_histoire flower cat"><a href="#story">Histoire</a></li>
+    <li class="menu_ouvert_personnages flower cat"><a href="#characters">Personnages</a></li>
+    <li class="menu_ouvert_lieu flower cat"><a href="#place">Lieu</a></li>
+    <li class="menu_ouvert_studio flower cat"><a href="#studio">Studio Koukaki</a>
+    </li>
+</ul>
+<div class="menu_ouvert_footer modal-trigger flower cat">
+    <a href="#">STUDIO KOUKAKI</a></li>
 </div>
